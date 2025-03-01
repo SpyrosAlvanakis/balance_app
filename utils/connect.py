267eee43cf94b9d08b42b39be15fef1da7da40f0
SpyrosAlvanakis <s.alvanakis@gmail.com>
2025-03-01 21:43:20 +0200

    secrets

--- utils/connect.py
+++ utils/connect.py
@@ -1,18 +1,28 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import os 
-import dotenv
+import streamlit as st
 
-# load the environmet
-dotenv.load_dotenv(os.path.join('env_and_cred', '.env'))
-
-# @st.cache_data
 def connect():  
     """Connect to the Google Sheets API."""
     scope = ['https://spreadsheets.google.com/feeds',
              'https://www.googleapis.com/auth/drive']
+    
+    credentials_dct = {
+        "type": st.secrets['credentials']["type"],
+        "project_id": st.secrets['credentials']["project_id"],
+        "private_key_id": st.secrets['credentials']["private_key_id"],
+        "private_key": st.secrets['credentials']["private_key"],
+        "client_email": st.secrets['credentials']["client_email"],
+        "client_id": st.secrets['credentials']["client_id"],
+        "auth_uri": st.secrets['credentials']["auth_uri"],
+        "token_uri": st.secrets['credentials']["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets['credentials']["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets['credentials']["client_x509_cert_url"],
+        "universe_domain": st.secrets['credentials']["universe_domain"]
+    }
 
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('env_and_cred/credentials.json', scope)
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dct, scope)
     client = gspread.authorize(credentials)
-    sheet = client.open(os.getenv("sheet")).sheet1  # Access first sheet
+    sheet = client.open(st.secrets['credentials']["sheet"]).sheet1  # Access first sheet
     return sheet
